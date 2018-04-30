b7bd1490ab516ab73e3abd9447db862728ea4e81
Ugo Princewill <uggoprince@gmail.com>
2018-04-30 13:24:07 +0100

    modified [#156846525]

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import url from 'url';
-import multer from 'multer';
 import {index, add_meal, modify_meal, delete_meal} from '../controllers/mealsController';
 
 const router = express.Router();
-const upload = multer({dest: '/../public'})
 
 const getm = router.get('/', index);
 
