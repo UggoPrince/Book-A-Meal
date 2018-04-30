0b915410fc7b47b7a128e900b0749b74c41c481b
Ugo Princewill <uggoprince@gmail.com>
2018-04-30 13:09:19 +0100

    modified

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-//import bodyParser from 'body-parser';
-//import multer from 'multer';
 import index from './server/routes/index';
-import {getm, postm, putm, delm} from './server/routes/meals';
+import {getm, postm} from './server/routes/menu';
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/server/public')));
-app.use(express.static(path.join(__dirname, '/UI')));
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/', index);
-app.use('/api/v1/meals', getm);
-app.use('/api/v1/meals', postm);
-app.use('/api/v1/meals', putm);
+app.use('/api/v1/menu', getm);
+app.use('/api/v1/menu', postm);
 
 app.listen(8080);
